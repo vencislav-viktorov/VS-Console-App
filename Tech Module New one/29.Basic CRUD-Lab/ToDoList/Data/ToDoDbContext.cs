5900094cf895c44497d0b9f9f7bd12a07ef541cd
venci9403@gmail.com <vencislav@1996>
2019-02-15 06:12:42 +0200

    Add new project

---
+++ Tech Module New one/29.Basic CRUD-Lab/ToDoList/Data/ToDoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=localhost\SQLEXPRESS01;Database=ToDoList;Trusted_Connection=True;";
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public virtual DbSet<Task> Tasks { get; set; }
+    }
+}
